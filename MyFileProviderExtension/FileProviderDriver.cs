d7027f77a2fabc6917d91a2d3d22af5729edb09e
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2022-07-13 13:03:51 -0700

    Initial commit

---
+++ MyFileProviderExtension/FileProviderDriver.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using FileProvider;
+using Foundation;
+using ObjCRuntime;
+
+namespace MyFileProviderExtension
+{
+    [Register("FileProviderDriver")]
+    public class FileProviderDriver: NSExtensionRequestHandling, INSFileProviderReplicatedExtension
+    {
+        [Export("initWithDomain:")]
+        public FileProviderDriver()
+        {
+        }
+
+        public override void BeginRequestWithExtensionContext(NSExtensionContext context)
+        {
+        }
+
+
+        public NSProgress CreateItem(INSFileProviderItem itemTemplate, NSFileProviderItemFields fields, NSUrl url, NSFileProviderCreateItemOptions options, NSFileProviderRequest request, NSFileProviderCreateOrModifyItemCompletionHandler completionHandler)
+        {
+            return null;
+        }
+
+        public NSProgress DeleteItem(string identifier, NSFileProviderItemVersion version, NSFileProviderDeleteItemOptions options, NSFileProviderRequest request, Action<NSError> completionHandler)
+        {
+            return null;
+        }
+
+
+        public NSProgress FetchContents(string itemIdentifier, NSFileProviderItemVersion requestedVersion, NSFileProviderRequest request, NSFileProviderFetchContentsCompletionHandler completionHandler)
+        {
+            return null;
+        }
+
+        public INSFileProviderEnumerator GetEnumerator(string containerItemIdentifier, NSFileProviderRequest request, out NSError error)
+        {
+            error = NSError.FromDomain(new NSString("NSFileProviderErrorDomain"),
+                (int)NSFileProviderError.NotAuthenticated, null);
+
+            return null;
+        }
+
+        public NSProgress GetItem(string identifier, NSFileProviderRequest request, Action<INSFileProviderItem, NSError> completionHandler)
+        {
+            return null;
+        }
+
+        public void Invalidate()
+        {
+        }
+
+        public NSProgress ModifyItem(INSFileProviderItem item, NSFileProviderItemVersion version, NSFileProviderItemFields changedFields, NSUrl newContents, NSFileProviderModifyItemOptions options, NSFileProviderRequest request, NSFileProviderCreateOrModifyItemCompletionHandler completionHandler)
+        {
+            return null;
+        }
+    }
+}
+
---
+++ XamarinMacFileProviderExample/AppDelegate.cs
@@ -0,0 +1,107 @@
+﻿using AppKit;
+using FileProvider;
+using Foundation;
+
+namespace XamarinMacFileProviderExample
+{
+	[Register ("AppDelegate")]
+	public class AppDelegate : NSApplicationDelegate
+	{
+		private NSFileProviderDomain fileProviderDomain;
+		private NSFileProviderManager fileProviderManager;
+
+		public AppDelegate ()
+		{
+			this.fileProviderDomain = new NSFileProviderDomain(SharedIdentifiers.FileProviderDomains.primaryDomainIdentifier, SharedIdentifiers.FileProviderDomains.primaryDomainDisplayName);
+		}
+
+		public override void DidFinishLaunching (NSNotification notification)
+		{
+			Mount();
+		}
+
+		public override void WillTerminate (NSNotification notification)
+		{
+			// Insert code here to tear down your application
+		}
+
+		public void Mount()
+		{
+			NSFileProviderManager.GetDomains((domains, error) =>
+			{
+				if (error != null)
+				{
+					RegisterDomain();
+					return;
+				}
+
+				bool foundDomain = false;
+
+				foreach (NSFileProviderDomain domain in domains)
+				{
+					if (domain.Identifier.Equals(fileProviderDomain.Identifier) && domain.DisplayName.Equals(fileProviderDomain.DisplayName))
+					{
+						foundDomain = true;
+					}
+					else
+					{
+					}
+				}
+
+				if (foundDomain)
+				{
+					// TODO: follow-up CR brings this: ReEnumerateWorkingSet();
+					return;
+				}
+				else
+				{
+					RegisterDomain();
+				}
+			});
+		}
+
+		private void RegisterDomain()
+		{
+			NSFileProviderManager.AddDomain(fileProviderDomain, (NSError error) =>
+			{
+				if (error != null)
+				{
+					System.nint code = error.Code;
+					return;
+
+					/*
+                    enum NSFileProviderError : long
+		            NotAuthenticated = -1000,
+		            FilenameCollision = -1001,
+		            SyncAnchorExpired = -1002,
+		            PageExpired = SyncAnchorExpired,
+		            InsufficientQuota = -1003,
+		            ServerUnreachable = -1004,
+		            NoSuchItem = -1005,
+		            VersionOutOfDate = -1006,
+		            DirectoryNotEmpty = -1007,
+		            ProviderNotFound = -2001,
+		            ProviderTranslocated = -2002,
+		            OlderExtensionVersionRunning = -2003,
+		            NewerExtensionVersionFound = -2004,
+		            CannotSynchronize = -2005,
+		            NonEvictableChildren = -2006,
+		            UnsyncedEdits = -2007,
+		            NonEvictable = -2008,
+		            VersionNoLongerAvailable = -2009,
+                    */
+				}
+			});
+		}
+	}
+
+	static public class SharedIdentifiers
+	{
+		static public class FileProviderDomains
+		{
+			static public string primaryDomainIdentifier = "com.hgindra.xamarinmacfileproviderexample";
+			static public string primaryDomainDisplayName = "Documents";
+		}
+	}
+}
+
